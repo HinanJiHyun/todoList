ca23ad7373f6be88ba68dff569df2f235a587a1d
HinanJiHyun <wlgusdldi15@naver.com>
2025-07-13 01:59:59 +0900

    todo List searchBar implementation

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,36 @@
 import './List.css';
 import TodoItem from './TodoItem.jsx';
+import { useState } from 'react';
+
 const List = ({ todos }) => {
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) => {
+      return todo.content.toLowerCase().includes(search.toLowerCase());
+    });
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className='List'>
       <h4>Todo List 🍏</h4>
-      <input type='text' placeholder='검색어를 입력하세요' />
+      <input
+        type='text'
+        placeholder='검색어를 입력하세요'
+        onChange={onChangeSearch}
+        value={search}
+      />
       <div className='todos_wrapper'>
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return <TodoItem {...todo} key={todo.id} />;
         })}
       </div>

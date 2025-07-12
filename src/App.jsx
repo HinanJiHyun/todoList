f43912a04379e360f48aeb7b1e129cd7ffa1afc5
jijyunnn <jijyunnn@mz.co.kr>
2025-07-12 23:27:42 +0900

    todo List 기능 구현을 위한 App.jsx 초기 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import './App.css'
+import {useState} from "react";
 import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
 
+// 변하지 않는 데이터는 컴포넌트 외부에 생성하는게 좋음
+const mockData=[
+  {
+    id:0,
+    isDone: false,
+    content: "React 공부",
+    date: new Date().getTime(),
+  },
+  {
+    id:1,
+    isDone: false,
+    content: "출근하기",
+    date: new Date().getTime(),
+  },
+  {
+    id:2,
+    isDone: false,
+    content: "퇴근하기",
+    date: new Date().getTime(),
+  }
+]
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
 
   return (
     <div className="App">

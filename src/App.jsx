0a63c35af08fcf598cec2db6dbfe0e8223717f50
HinanJiHyun <wlgusdldi15@naver.com>
2025-07-14 00:28:37 +0900

    todo List useReducer로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useReducer } from 'react';
 import Header from './components/Header.jsx';
 import Editor from './components/Editor.jsx';
 import List from './components/List.jsx';
@@ -26,45 +26,49 @@ const mockData = [
   },
 ];
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((todo) =>
+        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo,
+      );
+    case 'DELETE':
+      return state.filter((todo) => todo.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
-  const isRef = useRef(3);
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: isRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
   const onUpdate = (targetId) => {
-    // todo State의 값들 중에
-    // targetId와 일치하는 id를 갖는 todoItem의 isDone 을 변경
-    // 인수: todos배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열을 리턴
-
-    // setTodos(
-    //   todos.map((todo) => {
-    //     if (todo.id === targetId) {
-    //       return {
-    //         ...todo,
-    //         isDone: !todo.isDone,
-    //       };
-    //     }
-    //     return todo;
-    //   }),
-    // );
-
-    setTodos(
-      todos.map((todo) => (todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo)),
-    );
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId,
+    });
   };
 
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId,
+    });
   };
 
   return (

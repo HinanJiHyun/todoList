0babcac1073843ad137a3f04f730ce4d4d9d59ce
HinanJiHyun <wlgusdldi15@naver.com>
2025-07-14 20:29:43 +0900

    memo를 이용해 코드 최적화 및 memo customizing example

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-import './Header.css'
+import './Header.css';
+import { memo } from 'react';
 const Header = () => {
-  return(
-    <div className="Header">
+  return (
+    <div className='Header'>
       <h3>오늘은 📆</h3>
       <h1>{new Date().toDateString()}</h1>
     </div>
-  )
-}
-export default Header
+  );
+};
+
+// 자신이 받는 props가 변경되지않으면 리렌더링이 발생하지 않음
+// const memoizedHeader = memo(Header);
+// export default memoizedHeader;
+
+export default memo(Header);
+// export default Header;

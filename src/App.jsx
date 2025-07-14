fcad38c3ebb44ae07623c0472cba4e0445f9757a
HinanJiHyun <wlgusdldi15@naver.com>
2025-07-14 22:45:50 +0900

    context 분리 및 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useRef, useReducer, useCallback, createContext } from 'react';
+import { useState, useRef, useReducer, useCallback, createContext, useMemo } from 'react';
 import Header from './components/Header.jsx';
 import Editor from './components/Editor.jsx';
 import List from './components/List.jsx';
@@ -43,8 +43,8 @@ function reducer(state, action) {
 
 // context는 컴포넌트 외부에 선언한다
 // 외부에서 사용하기 위해 export
-export const TodoContext = createContext();
-console.log('TodoContext', TodoContext);
+export const TodoStateContext = createContext();
+export const TodoDispatchContext = createContext();
 
 function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
@@ -76,20 +76,23 @@ function App() {
     });
   }, []);
 
+  const memoizedDispatch = useMemo(() => {
+    return { onCreate, onUpdate, onDelete };
+  }, []);
+
   return (
     <div className='App'>
       <Header />
-      <TodoContext.Provider
-        value={{
-          todos,
-          onCreate,
-          onUpdate,
-          onDelete,
-        }}
-      >
-        <Editor />
-        <List />
-      </TodoContext.Provider>
+      <TodoStateContext.Provider value={todos}>
+        <TodoDispatchContext.Provider
+          // App컴포넌트의 todos State가 변경이 되어서 리렌더링이 일어나게 되면 해당 함수들도 재생성 되게 됨
+          // useMemo를 사용
+          value={memoizedDispatch}
+        >
+          <Editor />
+          <List />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
     </div>
   );
 }
